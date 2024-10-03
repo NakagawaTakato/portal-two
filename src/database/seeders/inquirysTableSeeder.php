09dc0fd94150a14089f7d65b9443856aeefdc228
NakagawaTakato <ntakato6231@gmail.com>
2024-10-03 17:15:52 +0900

    4

--- src/database/seeders/inquirysTableSeeder.php
+++ src/database/seeders/inquirysTableSeeder.php
@@ -2,19 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Inquiry;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class inquirysTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            inquirysTableSeeder::class
-        ]);
+        Inquiry::factory()->count(35)->create();
     }
 }
